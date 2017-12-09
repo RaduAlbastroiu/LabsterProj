faef8997245bbaf0ca8fa42e8aac7567085b2132
Radu Albastroiu <ducu@MacDuc.local>
2017-12-09 21:19:32 +0200

    Database set

--- LabsterProj/ViewController.swift
+++ LabsterProj/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // don't u fucking dare not to work
+        let ref = Database.database().reference()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-LabsterProj/Pods-LabsterProj-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LabsterProj : NSObject
+@end
+@implementation PodsDummy_Pods_LabsterProj
+@end
